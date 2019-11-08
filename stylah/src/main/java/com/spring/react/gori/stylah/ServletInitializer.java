b0e8c84134e8e1334de276e5bd6fb47d9b26f11c
Christian John Aparejado <christian.john.aparejado@oocl.com>
2019-11-08 09:50:43 +0800

    test

--- stylah/src/main/java/com/spring/react/gori/stylah/ServletInitializer.java
+++ stylah/src/main/java/com/spring/react/gori/stylah/ServletInitializer.java
@@ -1,5 +1,7 @@
 package com.spring.react.gori.stylah;
 
+import javafx.application.Application;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
@@ -10,4 +12,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 		return application.sources(StylahApplication.class);
 	}
 
+	public static void main(String[] args) throws Exception{
+		SpringApplication.run(Application.class, args);
+	}
 }
